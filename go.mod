838d18ca092e13758a9e3819eee9ceb24d141f83
fergusohalloranOTD <fergusohalloran@onthedoor.net>
2025-03-28 14:45:44 +0000

    Refactor to use RedisTTL for storing bearer tokens

--- go.mod
+++ go.mod
@@ -5,12 +5,15 @@ go 1.23.0
 toolchain go1.24.1
 
 require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx v3.6.2+incompatible // indirect
 	github.com/jackc/pgx/v5 v5.7.3 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/redis/go-redis/v9 v9.7.3 // indirect
 	golang.org/x/crypto v0.36.0 // indirect
 	golang.org/x/sync v0.12.0 // indirect
 	golang.org/x/text v0.23.0 // indirect
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"crypto/sha256"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -13,10 +14,9 @@ import (
 	"time"
 
 	"github.com/jackc/pgx"
-	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
 )
 
-const clientId string = "sandbox-rounder-ff20b2"
 const applicationName string = "rounder"
 const dataApiUrl string = "https://api.truelayer-sandbox.com/data"
 const tokenRefreshEndpoint string = "https://auth.truelayer-sandbox.com/connect/token"
@@ -39,15 +39,8 @@ type Transaction struct {
 }
 
 type User struct {
-	Id          int
-	AccessToken string
-	AccountCode string
-}
-
-type ExpiredTokenUser struct {
-	Id          int
-	AccountCode string
-	AccessCode  string
+	RefreshToken string
+	AccountCode  string
 }
 
 type TokenResponse struct {
@@ -67,168 +60,173 @@ func getUsers() ([]User, error) {
 		Port:     uint16(port),
 		Database: os.Getenv("DATABASE_NAME"),
 		User:     os.Getenv("DATABASE_USER"),
-		Password: os.Getenv("DATABASE_PASSWORD")}
+		Password: os.Getenv("DATABASE_POSTGRES_PASSWORD")}
 
 	conn, err := pgx.Connect(connection_config)
 	if err != nil {
-		return nil, fmt.Errorf("Unable to connect to database: %v\n", err)
+		return nil, fmt.Errorf("Unable to connect to database: %w\n", err)
 	}
 	defer conn.Close()
 
-	query := "SELECT id, access_token, account_code FROM \"Rounder\".users"
+	query := "SELECT account_code, refresh_token FROM \"Rounder\".users"
 	rows, err := conn.Query(query)
 
 	if err != nil {
-		return nil, fmt.Errorf("Query to database failed: %v\n", err)
+		return nil, fmt.Errorf("Query to database failed: %w\n", err)
 	}
 
 	var users []User
 
 	for rows.Next() {
 		var newUser User
-		err := rows.Scan(&newUser.Id, &newUser.AccessToken, &newUser.AccountCode)
+		err := rows.Scan(&newUser.AccountCode, &newUser.RefreshToken)
 		if err != nil {
-			return nil, fmt.Errorf("Invalid data from database retrieved: %v\n", err)
+			return nil, fmt.Errorf("Invalid data from database retrieved: %w\n", err)
 		}
 		users = append(users, newUser)
 	}
 	return users, nil
 }
 
-func getNewToken(){
+func refreshBearerToken(user User, rdb *redis.Client, ctx context.Context) error {
+	fmt.Printf("Refreshing token for AccountCode: %s\n", user.AccountCode)
+	requestData := url.Values{}
+
+	requestData.Set("grant_type", "refresh_token")
+	requestData.Set("client_id", os.Getenv("TL_CLIENT_ID"))
+	requestData.Set("client_secret", os.Getenv("CLIENT_SECRET"))
+	requestData.Set("refresh_token", user.RefreshToken)
+
+	payload := strings.NewReader(requestData.Encode())
+	req, err := http.NewRequest("POST", tokenRefreshEndpoint, payload)
+	if err != nil {
+		return fmt.Errorf("Error generating post request for token refresh: %w", err)
+	}
+
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("accept", "application/x-www-form-urlencoded")
+	response, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("error sending POST request for token refresh: %w", err)
+	}
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("Error code recieved when getting refresh token:", response.StatusCode)
+	}
+
+	bodyBytes, err := io.ReadAll(response.Body)
+	defer response.Body.Close()
+	if err != nil {
+		return fmt.Errorf("error reading response body of refresh token: %w", err)
+	}
+
+	var tokenResponse TokenResponse
+
+	err = json.Unmarshal(bodyBytes, &tokenResponse)
+	if err != nil {
+		return fmt.Errorf("Error decoding JSON:", err)
+
+	}
+	expiryTime := time.Duration(tokenResponse.ExpiresIn) * time.Second
+	hashedKey := sha256.Sum256([]byte(user.AccountCode))
+	err = rdb.Set(ctx, string(hashedKey[:]), tokenResponse.AccessToken, expiryTime).Err()
+	if err != nil {
+		return fmt.Errorf("Error setting new bearer token :", err)
+	}
+	return nil
+}
+
+func getBearerToken(user User, rdb *redis.Client) (string, error) {
+	var bearerToken string
+	ctx := context.Background()
+	hashedKeyBytes := sha256.Sum256([]byte(user.AccountCode))
+	hashedKey := string(hashedKeyBytes[:])
+	bearerToken, err := rdb.Get(ctx, hashedKey).Result()
+	if err == redis.Nil { // Bearer token has expired
+		err = refreshBearerToken(user, rdb, ctx)
+		if err != nil {
+			return "", fmt.Errorf("Error refreshing token for '%s': %w", user.AccountCode, err)
+		}
+		bearerToken, err = rdb.Get(ctx, hashedKey).Result()
+	} else if err != nil {
+		return "", fmt.Errorf("Error getting bearerToken for '%s': %w", user.AccountCode, err)
+	}
+
+	return bearerToken, nil
+}
+
+func getTransactions(user User, rdb *redis.Client) ([]Transaction, error) {
+	bearerToken, err := getBearerToken(user, rdb)
+	if err != nil {
+		return nil, fmt.Errorf("Error getting bearer token for user '%s': %w", user.AccountCode, err)
+	}
+	url := fmt.Sprintf("%s/%s/accounts/%s/transactions", dataApiUrl, apiVersion, user.AccountCode)
+	bearer := fmt.Sprintf("Bearer %s", bearerToken)
+
+	req, _ := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("Error generating GET request for transaction data : %w", err)
+	}
+
+	req.Header.Add("accept", "application/json")
+	req.Header.Add("authorization", bearer)
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("Error sending request to get transactions:", err)
+
+	}
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Error code recieved when getting transactions:", res.Status)
+
+	}
+
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+	var response ApiResponse
+	var transactions []Transaction
+	err = json.Unmarshal([]byte(body), &response)
+	if err != nil {
+		return nil, fmt.Errorf("Error parsing JSON:", err)
+	}
+	for _, transaction := range response.Results {
+		if transaction.Amount < 0 && transaction.Category == "PURCHASE" { //Filter out transactions in and non purchases TODO: see if this can be filtered in the API call to reduce data pulled
+			transactions = append(transactions, transaction)
+
+		}
+
+	}
+	return transactions, nil
 
 }
 
-func refreshTokens() error {
-	fmt.Println("Refresh tokens started")
-	
-	dbURL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
-	os.Getenv("DATABASE_USER"),
-	os.Getenv("DATABASE_PASSWORD"),
-	os.Getenv("DATABASE_HOST"),
-	os.Getenv("DATABASE_PORT"),
-	os.Getenv("DATABASE_NAME"),
-	)
-	fmt.Println(dbURL)
-
-	pool, err := pgxpool.New(context.Background(), dbURL)
-	if err != nil {
-		return fmt.Errorf("Unable to connect to database: %v\n", err)
-	}
-	defer pool.Close()
-
-	ctx := context.Background()
-
-	query := "SELECT id, account_code, access_code FROM \"Rounder\".users WHERE token_expiry_date < now()"
-	rows, err := pool.Query(ctx,query)
-
-	if err != nil {
-		return fmt.Errorf("Query to database failed: %v\n", err)
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var expiredUser ExpiredTokenUser
-		err := rows.Scan(&expiredUser.Id, &expiredUser.AccountCode, &expiredUser.AccessCode)
-		if err != nil {
-			return fmt.Errorf("Invalid data from database retrieved when getting expired tokens: %v\n", err)
-		}
-		data := url.Values{}
-
-		data.Set("grant_type", "authorization_code")
-		data.Set("client_id", clientId)
-		data.Set("client_secret", os.Getenv("CLIENT_SECRET"))
-		data.Set("redirect_uri", "https://console.truelayer.com/redirect-page")
-		data.Set("code", expiredUser.AccessCode)
-
-		payload := strings.NewReader(data.Encode())
-		req, _ := http.NewRequest("POST", tokenRefreshEndpoint, payload)
-
-		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-		fmt.Println(req)
-		req.Header.Add("accept", "application/x-www-form-urlencoded")
-		response, err := http.DefaultClient.Do(req)
-		bodyBytes, _ := io.ReadAll(response.Body)
-
-		if err != nil {
-			fmt.Println(string(bodyBytes))
-			return fmt.Errorf("Error sending request to get token:", err)
-
-		}
-		if response.StatusCode != 200 {
-			fmt.Println(string(bodyBytes))
-			return fmt.Errorf("Error code recieved when getting token:", err)
-		}
-		defer response.Body.Close()
-		var tokenResponse TokenResponse
-
-		err = json.Unmarshal(bodyBytes, &tokenResponse)
-		if err != nil {
-			return fmt.Errorf("Error decoding JSON:", err)
-
-		}
-		expiryTime := time.Now()
-		expiryTime = expiryTime.Add(time.Second * time.Duration(tokenResponse.ExpiresIn))
-		expiryTimeStr := expiryTime.Format(time.RFC3339)
-		query = fmt.Sprintf("UPDATE \"Rounder\".users SET access_token = '%s', access_code='%s', token_expiry_date='%s' WHERE id='%s'", tokenResponse.AccessToken, tokenResponse.RefreshToken, expiryTimeStr,strconv.Itoa(expiredUser.Id))
-		fmt.Println(query)
-		_, err = pool.Exec(ctx,query)
-		if err != nil {
-			return fmt.Errorf("Query failed:", err)
-		}
-	}
-
+func pushTransactions(transactions []Transaction) error {
 	return nil
 }
 
 func main() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("DATABASE_REDIS_ADDRESS"),
+		Username: os.Getenv("DATABASE_REDIS_USER"),
+		Password: os.Getenv("DATABASE_REDIS_PASSWORD"),
+		DB:       0, // Use default DB
+	})
 
-	err := refreshTokens()
-	if err != nil {
-		fmt.Println("Error refreshing tokens:", err)
-		return
-	}
-	fmt.Println("Refresh tokens finished")
 	users, err := getUsers()
 	if err != nil {
 		fmt.Println("Error getting users from database:", err)
 		return
 	}
 	for _, user := range users {
-		url := fmt.Sprintf("%s/%s/accounts/%s/transactions", dataApiUrl, apiVersion, user.AccountCode)
-		bearer := fmt.Sprintf("Bearer %s", user.AccessToken)
-
-		req, _ := http.NewRequest("GET", url, nil)
-
-		req.Header.Add("accept", "application/json")
-		req.Header.Add("authorization", bearer)
-		res, err := http.DefaultClient.Do(req)
+		transactions, err := getTransactions(user, rdb)
 		if err != nil {
-			fmt.Println("Error sending request to get transactions:", err)
-			return
+			fmt.Println("Error getting transactions data:", err)
+			continue
 		}
-		if res.StatusCode != 200 {
-			fmt.Println("Error code recieved when getting transactions:", res.Status)
-			return
-		}
-
-		defer res.Body.Close()
-		body, _ := io.ReadAll(res.Body)
-		var response ApiResponse
-		err = json.Unmarshal([]byte(body), &response)
+		fmt.Println(transactions)
+		err = pushTransactions(transactions)
 		if err != nil {
-			fmt.Println("Error parsing JSON:", err)
-			return
+			fmt.Println("Error pushing transactions to kafka:", err)
+			continue
 		}
-		for _, transaction := range response.Results {
-			if transaction.Amount < 0 && transaction.Category == "PURCHASE" { //Filter out transactions in and non purchases TODO: see if this can be filtered in the API call to reduce data pulled
-				fmt.Println(transaction)
-				//Push transactions to "new_transactions" kafka topic with UserID, Amount, Timestamp
-
-			}
-
-		}
-
 	}
 
 }
